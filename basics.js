f77696665f6f58fe25496a4d8051b20e3ebe0bdd
Raymond Mar <raym61@hotmail.com>
2024-08-09 14:22:36 -0700

    Variables, objects, conditionals, loops

--- basics.js
+++ basics.js
@@ -151,3 +151,250 @@ let [leftvariablex, rightvariabley] = [1,2];
 [leftvariablex,rightvariabley] = [rightvariabley,leftvariablex];
 console.log(leftvariablex) //print 224
 console.log(rightvariabley) //print 112
+
+/* JavaScript The Definitive Guide By David Flanagan Chapter 04 Expressions And Operators */
+let anemptyarray = [];
+let twoelementarray = [1+2,3+4];
+let nestedarrays = [[1,2,3],[4,5,6],[7,8,9]];
+console.log(nestedarrays)
+/*
+0 : Array(3)
+0: 1
+1: 2
+2: 3
+length: 3
+    [[Prototype]] : Array(0)
+1 : (3) [4, 5, 6]
+2 : (3) [7, 8, 9]
+length : 3
+*/
+let fiveelementsthreeundefined = [1,,,5];
+let objectexpression = {x:2.3, y:-1.2};
+let emptyobjectexpression = {}
+console.log(emptyobjectexpression) //print {}
+emptyobjectexpression.x=2.3, emptyobjectexpression.y=-1.2;
+console.log(emptyobjectexpression)
+/*
+{x: 2.3, y: -1.2}
+x : 2.3
+y : -1.2
+*/
+let objectexample = {x:1, y: {z:3}};
+let arrayexample = [objectexample, 4, [5,6]];
+console.log(objectexample.x) //print 1
+console.log(objectexample.y.z) //print 3
+console.log(objectexample["x"]) //print 1
+console.log(arrayexample[1]) //print 4
+console.log(arrayexample[2][1]) //print 6
+console.log(arrayexample[0].x) //print 1
+/*
+Selected Operators
+++ increment
+-- decrement
+** exponents
++ concatenate strings
+instanceof tests object class
+in tests whether property exists
+== non-strict equality or is equal to
+!= non-strict inequality
+=== strict equality or is strictly equal to.  Author says use === instead of ==.
+!== strict inequality.  Author says use !== instead of !=.
+&& logical and
+|| logical or
+*/
+console.log("combine "+ "these " + "strings "+ "with "+ "plus "+ "sign +") //print combine these strings with plus sign +
+console.log("1"+2) //print 12.  number converts to string.
+console.log(typeof("1"+2)) //print string
+console.log(1+2+" blind mice") //print 3 blind mice
+console.log(true+true) //print 2.  True is numerical value 1.
+console.log(2+null) //print 2.  Null converts to numerical value 0.
+let dobject = new Date();
+console.log(dobject instanceof Date) //print true.  dobject created with Date().
+console.log(dobject instanceof Object) //print true.  dobject is an object for which all objects are an instace of Object.
+console.log(dobject instanceof Number) //print false.  dobject is not a Number object.
+let aarray = [1,2,3];
+console.log(aarray instanceof Array) //print true. aarray is an array.
+console.log(aarray instanceof Object) //print true. aarray is an array for which all arrays are objects.
+console.log(aarray instanceof RegExp) //print false. aarray is an array not a regular expression.
+/* Global function eval() interprets strings to produce a value. It can be a security problem.  Don't pass string from user input. */
+console.log(eval("3+2")) //print 5
+console.log(eval("xlocalvariable=1")) //print 1
+console.log(xlocalvariable) //print 1
+console.log(eval("var ylocalvariable=3;")) //print undefined
+console.log(ylocalvariable) //print 3
+/* delete removes an object property or array element. */
+let oobject = {x:1, y:2};
+console.log(oobject) //print {x: 1, y: 2}
+delete oobject.x
+console.log(oobject) //print {y: 2}
+let aarray2 = [1,2,3];
+console.log(aarray2) //print [1, 2, 3]
+delete aarray2[2];
+console.log(3 in aarray2) //print false
+
+/* JavaScript The Definitive Guide By David Flanagan Chapter 05 Statements */
+/*Conditionals.  If statements.  A single statement after the if is required.
+if (expression)
+    statement1    
+else
+    statement2
+
+if (expression) statement
+
+if (expression1) {
+    statement1 {
+    if (expressionnestedif2)
+        statementnestedif2
+    }
+    } else {
+        statement3
+    }
+
+if (expression1) {
+    statement1
+} else if (expression2) {
+    statement2
+} else if (expression3) {
+    statement3
+} else {
+    statement4
+}
+*/
+let username = "full";
+if (username == null)
+    username = "John Doe";
+//or
+if (!username) username = "John Doe";
+
+let n = 58;
+if (n===1)
+    variable1 = "n is one";
+else
+    variable1 = "n is not one";
+
+let elseifvariable = "111";
+if (elseifvariable === "000") {
+    console.log("Number 000");
+} else if (elseifvariable === "111") {
+    console.log("Number 111"); //print Number 111
+} else if (elseifvariable === "222") {
+    console.log("Number 222");
+} else {
+    console.log ("Number unknown");
+}
+
+/*Switch.
+
+switch(expression) {
+    statements
+}
+*/
+let nn = 59;
+switch(nn) {
+    case 49:
+        console.log("nn is 49")
+        break;
+    case 55:
+        console.log("nn is 55")
+        break;
+    case 59:
+        console.log("nn is 59") //print nn is 59
+        break;
+    case 1793:
+        console.log("nn is 1793")
+        break;
+    default:
+        console.log("Without break; statement, switch prints nn is 59 nn is 1693 and the default statement")
+        break;
+}
+/*While
+while (expression)
+    statement
+
+Do while
+do
+    statement
+while (expression);
+
+*/
+let count = 0
+while(count < 10) {
+    console.log(count); //print from 0 to 9 in its own line
+    count++;
+}
+let numbervariable = 0
+do {
+    console.log(numbervariable); //print from 0 to 10 in its own line
+} while (numbervariable++ < 10);
+
+/*For
+for(all the initialize ; all the test ; all the increment)
+    statement
+
+for of
+for (let variable1 of variable2) {
+    console.log("for/of loop works with iterable objects such as arrays, strings, sets, and maps.")
+}
+
+for in
+for (variable in object)
+    console.log("for/in is like for/of loop.  for/of loop requires an interable object.  for/in works with any object.")
+Author says when working with arrays, you almost always want to use for/of instead of for/in.  The for/in loop does not actually enumerate all properties of an object.
+*/
+for(let count2 = 0; count2 < 10; count2++) {
+    console.log(count2) //print from 0 to 9 in its own line.  RM:  semicolons in the for statement.
+}
+let ivariable, jvariable, sumvariable = 0;
+for (ivariable=0,jvariable=10; ivariable< 10; ivariable++, jvariable--) {
+    sumvariable += ivariable * jvariable
+    console.log("Example multiple initialize, test, and increment "+sumvariable)
+    /*
+    Example multiple initialize, test, and increment 0
+    Example multiple initialize, test, and increment 9
+    Example multiple initialize, test, and increment 25
+    Example multiple initialize, test, and increment 46
+    Example multiple initialize, test, and increment 70
+    Example multiple initialize, test, and increment 95
+    Example multiple initialize, test, and increment 119
+    Example multiple initialize, test, and increment 140
+    Example multiple initialize, test, and increment 156
+    Example multiple initialize, test, and increment 165
+    */
+}
+for (let onelongword of "thequickbrown") {
+    console.log(onelongword) //print thequickbrown each letter in its own line
+}
+let pythondictionary = {x:"one", y:"two", z:"three"};
+let eachk = "";
+for (let eachk of Object.keys(pythondictionary)) {
+    console.log(eachk) //print xyz in its own line
+}
+let eachv = "";
+for (let eachv of Object.values(pythondictionary)) {
+    console.log(eachv) //print one two three in its own line
+}
+for (let [eachkey, eachvalue] of Object.entries(pythondictionary)) {
+    console.log(eachkey, eachvalue) //print x one\n y two\n z three\n
+}
+let countletters = {};
+for (let eachletter of "mississippi") {
+    if (countletters[eachletter]) {
+        countletters[eachletter]++;
+    } else {
+        countletters[eachletter] = 1;
+    }
+}
+console.log(countletters) //print {m: 1, i: 4, s: 4, p: 2}
+for (let eachletter of "oneleterword") {
+    console.log(eachletter) //print oneleterword in its own line.  RM:  the tt returns a number 2 which is printed 2t if oneletterword
+}
+for (let eachletter in "oneleterword") {
+    console.log(eachletter) //print from 0 to 11 in its own line
+}
+wordsvariable = "a simple idea"
+for (let eachletter of wordsvariable) {
+    console.log(eachletter) //print a simple idea in its own line with the spaces
+}
+for (let eachwordsvariable in wordsvariable){
+    console.log(eachwordsvariable) //print from 0 to 11 in its own line
+}
