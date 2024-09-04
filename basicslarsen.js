a86d5dab3cc8b893dbc4195f5e5e4125ec0a18ae
Raymond Mar <raym61@hotmail.com>
2024-09-04 13:21:16 -0700

    Read new book Get Programming With JavaScript By John Larsen

---
+++ basicslarsen.js
@@ -0,0 +1,16 @@
+/* Get Programming With JavaScript By John Larsen Chapter 2 Variables: Storing Data In Your Program */
+var varkeyworddeclarevariable;
+varkeyworddeclarevariable = "Assign a string value to variable varkeyworddeclarevariable.";
+console.log(varkeyworddeclarevariable) //print Assign a string value to variable varkeyworddeclarevariable.
+var declarevariableassignvalue = "Declare variable and assign a value in one line.";
+console.log(declarevariableassignvalue) //print Declare variable and assign a value in one line.
+console.log("Use addition symbol to join text strong concatenation " +varkeyworddeclarevariable+ " " +declarevariableassignvalue) //print Use addition symbol to join text strong concatenation Assign a string value to variable varkeyworddeclarevariable. Declare variable and assign a value in one line.
+var addscore;
+addscore = 100+50;
+console.log(addscore); //print 150
+var expressionnumber1;
+expressionnumber1 = 111;
+var expressionnumber2 = 222;
+var expressionnumber3 = 499;
+totalexpression = expressionnumber1 + (expressionnumber2*expressionnumber3);
+console.log(totalexpression); //print 110889
