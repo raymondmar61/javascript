2d7fff9e0183bf044ea9ee127f837abb5600db99
Raymond Mar <raym61@hotmail.com>
2024-09-23 13:17:04 -0700

    Arrays or lists, some common array methods

--- basicslarsen.js
+++ basicslarsen.js
@@ -288,3 +288,95 @@ console.log(("convert lower case to upper case").toUpperCase()) //print CONVERT 
 console.log(("Extract string start count at zero start 3 get 14 characters").substr(3,14)) //print ract string st
 console.log(("Start count at zero find letter m The cow jumped over the Moon!").indexOf("M")) //print 58
 console.log(("Start count at zero find letter The cow jumped over the Moon! there is no no_").indexOf("not")) //print -1
+
+/* Get Programming With JavaScript By John Larsen Chapter 08 Arrays:  Putting Data Into Lists */
+let declarearrayvariable;
+declarearrayvariable = ["this", "is", "an", "array or list"];
+console.log(declarearrayvariable) //print ['this', 'is', 'an', 'array or list']
+/*
+0 : "this"
+1 : "is"
+2 : "an"
+3 : "array or list"
+length : 4
+*/
+let vacation1 = {name:"The Pyramids",country:"Egypt"};
+let vacation2 = {name:"The Grand Canyon",country:"USA"};
+let vacation3 = {name:"Bondi Beach",country:"Australia"};
+let traveltoday = [vacation1, vacation2];
+let traveltomorrow = [vacation3];
+console.log(traveltoday)
+/*
+0: {name: 'The Pyramids', country: 'Egypt'}
+1: {name: 'The Grand Canyon', country: 'USA'}
+*/
+console.log(traveltomorrow) //print {name: 'Bondi Beach', country: 'Australia'}
+let scoresindexing = [358, 136, 803, 247, 862];
+console.log(scoresindexing.length) //print 5
+console.log(scoresindexing[1]) //print 136
+let arrayinfunction = function (numberofvisitorsarray, dayintheweek) {
+    let days = ["Mon","Tue","Wed","Thur","Fri"];
+    let daynumber = dayintheweek-1;
+    let visitorreport;
+    visitorreport = "There were " +numberofvisitorsarray[daynumber]+ " visitors on " +days[daynumber];
+    return visitorreport;
+}
+let numberofvisitorsarray = [354,132,210,221,481];
+let displayreport = arrayinfunction(numberofvisitorsarray,2);
+console.log(displayreport) //print There were 132 visitors on Tue.  RM:  Get the Tue second day index number one visitors number.
+let somecommonarraymethods = ["assign","functions","to object properties","called methods","arrays are an object","their funtions are also called methods"]
+console.log(somecommonarraymethods) //print ['assign', 'functions', 'to object properties', 'called methods', 'arrays are an object', 'their funtions are also called methods']
+somecommonarraymethods.push("Insert at end of array.  Insert last.")
+console.log(somecommonarraymethods) //print ['assign', 'functions', 'to object properties', 'called methods', 'arrays are an object', 'their funtions are also called methods', 'Insert at end of array.  Insert last.']
+somecommonarraymethods.pop();
+console.log(somecommonarraymethods) //print ['assign', 'functions', 'to object properties', 'called methods', 'arrays are an object', 'their funtions are also called methods']
+concatenatearray = somecommonarraymethods.join(";"); //combine elements
+console.log(concatenatearray) //print assign;functions;to object properties;called methods;arrays are an object;their funtions are also called methods
+extractelements = somecommonarraymethods.slice(2,5);  //index numbers slice(inclusive,exclusive or inclusive-1)
+console.log(extractelements) //print ['to object properties', 'called methods', 'arrays are an object']
+console.log(somecommonarraymethods.slice(0,2)) //print ['assign', 'functions']
+console.log(somecommonarraymethods.slice(2,5)) //print ['to object properties', 'called methods', 'arrays are an object']
+console.log(somecommonarraymethods.slice(3)) //print ['called methods', 'arrays are an object', 'their funtions are also called methods']
+addelements = somecommonarraymethods.splice(3,2,"Add or remove consecutive elements.  Starting index number, number of elements to remove, any elements to add"); //RM:  replace elements with the string in its place.  Replace element starting an index 3 replace index 3 and replace index 4 wwhich are the 2 with Add or remove consecutive . . . .  Splice changes the original array removing and replacing.  To remove items, specify the index of the first element to remove and the number of elements to remove. The method returns the removed elements as an array.
+console.log(addelements) //print ['called methods', 'arrays are an object'] RM:  return the two elements to be replaced with one element Add or remove consecutive . . . .
+console.log(somecommonarraymethods) //print ['assign', 'functions', 'to object properties', 'Add or remove consecutive elements.  Starting inde…number of elements to remove, any elements to add', 'their funtions are also called methods']
+console.log(somecommonarraymethods.splice(3,2)) //print ['Add or remove consecutive elements.  Starting inde…number of elements to remove, any elements to add', 'their funtions are also called methods'].  RM:  these two elements are replaced with nothing.
+console.log(somecommonarraymethods) //print ['assign', 'functions', 'to object properties'].  RM:  The elements after removing elements starting from third index position and remove the next two elements.
+console.log(somecommonarraymethods.splice(2,1,"Replace with one string","Replace with a second string if I wanted")) //print ['to object properties'].  RM:  returns the elements to be replaced.
+console.log(somecommonarraymethods) //print ['assign', 'functions', 'Replace with one string', 'Replace with a second string if I wanted']  RM:  The elements replaced from the splice(2,1,"Replace with . . .)
+somecommonarraymethods.splice(0,0,"Insert elements add elements at the beginning no elements removed")
+console.log(somecommonarraymethods) //print ['Insert elements add elements at the beginning no elements removed', 'assign', 'functions', 'Replace with one string', 'Replace with a second string if I wanted']
+somecommonarraymethods.splice(1,1,"Removed first element first index number with something else")
+console.log(somecommonarraymethods) //print ['Insert elements add elements at the beginning no elements removed', 'Removed first element first index number with something else', 'functions', 'Replace with one string', 'Replace with a second string if I wanted']
+let somecommonarraymethodsreset1 = ["assign","functions","to object properties","called methods","arrays are an object","their funtions are also called methods"]
+iteratedarray = function (somecommonarraymethodsreset1parameter, index, wholearray){
+    console.log(somecommonarraymethodsreset1parameter)
+    console.log("Index number " +index+ " from array: " +wholearray+".  index and wholearray are optional.")
+};
+somecommonarraymethodsreset1.forEach(iteratedarray);
+/*
+assign
+Index number 0 from array: assign,functions,to object properties,called methods,arrays are an object,their funtions are also called methods.  index and wholearray are optional.
+functions
+Index number 1 from array: assign,functions,to object properties,called methods,arrays are an object,their funtions are also called methods.  index and wholearray are optional.
+to object properties
+Index number 2 from array: assign,functions,to object properties,called methods,arrays are an object,their funtions are also called methods.  index and wholearray are optional.
+called methods
+Index number 3 from array: assign,functions,to object properties,called methods,arrays are an object,their funtions are also called methods.  index and wholearray are optional.
+arrays are an object
+Index number 4 from array: assign,functions,to object properties,called methods,arrays are an object,their funtions are also called methods.  index and wholearray are optional.
+their funtions are also called methods
+Index number 5 from array: assign,functions,to object properties,called methods,arrays are an object,their funtions are also called methods.  index and wholearray are optional.
+*/
+console.log(numberofvisitorsarray) //print [354, 132, 210, 221, 481].  sum is 1,398
+let foreachfunctionsumnumbers = function (numberslist, useoptionalindex) {
+    console.log("Inside foreachfunctionsumnumbers " +numberslist) //print Inside foreachfunctionsumnumbers 354,132,210,221,481
+    innnerfunctionparameter = numberslist;
+    let total = 0;
+    numberslist.forEach(function (innerfunctionparameternotused, iindex) {
+        total += numberslist[iindex];
+    });
+    return total;
+
+};
+console.log("Total visitors sum is " +foreachfunctionsumnumbers(numberofvisitorsarray)) //print Total visitors sum is 1398
